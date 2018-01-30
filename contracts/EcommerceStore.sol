858bc23a3869ee2602b704f12a1cdab38527aea6
JosephKevin <jkm2155@columbia.edu>
2018-01-30 17:44:26 -0500

    added addProductToStore and getProduct functions

--- contracts/EcommerceStore.sol
+++ contracts/EcommerceStore.sol
@@ -29,4 +29,22 @@ contract EcommerceStore {
     function EcommerceStore() public{
         productIndex = 0;
     }
+
+    function addProductToStore(uint id, string name, string category, string imageLink, string descLink,
+        uint auctionStartTime, uint auctionEndTime, uint startPrice, uint condition) public{
+        require(auctionStartTime < auctionEndTime);
+        productIndex += 1;
+        Product memory product = Product(id, name, category, imageLink, descLink, auctionStartTime, auctionEndTime,
+                                 startPrice, 0, 0, 0, 0, ProductStatus.Open, ProductCondition(condition));
+        stores[msg.sender][productIndex] = product;
+        productIdInStore[productIndex] = msg.sender;
+    }
+
+    function getProduct(uint productId) view public returns (uint, string, string, string, string, uint, uint, uint,
+        ProductStatus, ProductCondition) {
+        Product memory product = stores[productIdInStore[productId]][productId];
+        return (product.id, product.name, product.category, product.imageLink, product.descLink,
+                product.auctionStartTime, product.auctionEndTime, product.startPrice, product.status,
+                product.condition);
+    }
 }
